ce7aa1048b0be587c9944a74f18dd1e2ebd05a2e
신예주 <yejushin2000@gmail.com>
2024-11-21 16:33:34 +0900

    프로필 페이지 프론트_username연동

--- front/src/stores/counter.js
+++ front/src/stores/counter.js
@@ -9,7 +9,7 @@ export const useCounterStore = defineStore('counter', () => {
   const router = useRouter();  // Vue Router 인스턴스
 
   // 사용자 정보 상태
-  const userProfile = ref(null);
+  const userProfile = ref({username: ''});
 
   // 로그인 여부를 computed로 처리
   const isLogin = computed(() => token.value !== null);
@@ -53,7 +53,9 @@ export const useCounterStore = defineStore('counter', () => {
     .then((res) => {
       token.value = res.data.key;  // 로그인 성공 시 받은 토큰 저장
       localStorage.setItem('token', res.data.key);  // 토큰을 로컬 스토리지에 저장
-      fetchUserProfile();  // 로그인 후 프로필 정보 가져오기
+
+      userProfile.value = res.data.user  // 사용자 프로필 정보 가져오기
+
       console.log('로그인 성공');  // 로그인 성공 메시지 콘솔에 출력
       router.push({ name: 'mainpage' });  // 로그인 후 메인 페이지로 이동
     })
@@ -63,23 +65,5 @@ export const useCounterStore = defineStore('counter', () => {
     });
   };
 
-
-  // 사용자 프로필 정보 가져오기 함수
-  const fetchUserProfile = async () => {
-    if (!token.value) return null;
-
-    try {
-      const response = await axios.get(`${API_URL}/accounts/api/v1/profile/`, {
-        headers: { Authorization: `Token ${token.value}` }
-      });
-      userProfile.value = response.data;
-      return userProfile.value;
-    } catch (error) {
-      console.error('사용자 프로필 정보를 가져오는 데 실패했습니다:', error);
-      return null;
-    }
-  };
-
-
-  return { signUp, token, isLogin, logIn, API_URL, userProfile, fetchUserProfile };
+  return { signUp, token, isLogin, logIn, API_URL, userProfile };
 });
