cb9ee66727fd656a4df453b23b997dbbf3d6ad7f
aaange <ayung00@naver.com>
2024-11-22 14:58:25 +0900

    feat(user): 로그아웃 기능 연동 및 토큰 문제 해결
    - 로그아웃 프론트 연결
    - 새로고침 시 로그아웃 되는 문제 해결

--- front/src/stores/counter.js
+++ front/src/stores/counter.js
@@ -5,8 +5,11 @@ import { useRouter } from 'vue-router';
 
 export const useCounterStore = defineStore('counter', () => {
   const API_URL = 'http://127.0.0.1:8000';  // API URL
-  const token = ref(null);  // 로그인 상태를 나타내는 token 변수
+  const token = ref(localStorage.getItem('token') || null);  // 로그인 상태를 나타내는 token 변수
   const router = useRouter();  // Vue Router 인스턴스
+
+  // 사용자 정보 상태
+  const userProfile = ref({username: ''});
 
   // 로그인 여부를 computed로 처리
   const isLogin = computed(() => token.value !== null);
@@ -47,16 +50,40 @@ export const useCounterStore = defineStore('counter', () => {
 
     axios.post(`${API_URL}/accounts/api/v1/login/`, { username, password })
     .then((res) => {
-      token.value = res.data.key;  // 로그인 성공 시 받은 토큰 저장
-      localStorage.setItem('token', res.data.key);  // 토큰을 로컬 스토리지에 저장
-      console.log('로그인 성공');  // 로그인 성공 메시지 콘솔에 출력
-      router.push({ name: 'mainpage' });  // 로그인 후 메인 페이지로 이동
+      console.log(res.data);
+      if (res.data.key) {
+        token.value = res.data.key;  // 로그인 성공 시 받은 토큰 저장
+        localStorage.setItem('token', res.data.key);  // 토큰을 로컬 스토리지에 저장
+  
+        userProfile.value = res.data.user  // 사용자 프로필 정보 가져오기
+  
+        console.log('로그인 성공');  // 로그인 성공 메시지 콘솔에 출력
+        console.log(res.data)
+        router.push({ name: 'mainpage' });  // 로그인 후 메인 페이지로 이동
+      } else {
+        console.error('토큰이 없거나 잘못된 응답을 받았습니다.')
+      }
     })
     .catch((err) => {
       console.log('로그인 실패:', err.response || err.message);  // 로그인 실패 시 콘솔에 오류 메시지 출력
       alert('로그인에 실패했습니다. 아이디와 비밀번호를 확인해주세요.');
     });
   };
-
-  return { signUp, token, isLogin, logIn, API_URL };
+  // [추가기능] 로그아웃
+  const logOut = function () {
+    axios({
+      method: 'post',
+      url: `${API_URL}/accounts/api/v1/logout/`, // 서버의 로그아웃 API 호출
+    })
+      .then((res) => {
+        console.log(res.data);
+        token.value = null; // 인증 토큰 비우기
+        localStorage.removeItem('token');  // 로컬 스토리지에서 토큰 삭제
+        router.push('/'); // 메인 페이지로 리디렉션
+      })
+      .catch((err) => {
+        console.log(err); // 에러 처리
+      });
+  };
+  return { signUp, token, isLogin, logIn, API_URL, userProfile, logOut };
 });
